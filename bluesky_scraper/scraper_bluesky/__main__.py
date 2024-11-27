bd5044512225e63933cf4511a7dcba66cc3fbcaa
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-11-27 18:54:24 -0500

    Working example of bluesky scraper
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

--- bluesky_scraper/scraper_bluesky/__main__.py
+++ bluesky_scraper/scraper_bluesky/__main__.py
@@ -2,12 +2,19 @@ import asyncio
 from playwright.async_api import async_playwright, Playwright
 
 
+URL = "https://bsky.app/"
+
+
 async def run(playwright: Playwright):
     chromium = playwright.chromium  # or "firefox" or "webkit".
     browser = await chromium.launch()
     page = await browser.new_page()
-    await page.goto("http://example.com")
-    # other actions...
+    await page.goto(URL)
+    
+    # Await the page title method.
+    title = await page.title()
+    print(title)
+
     await browser.close()
 
 
